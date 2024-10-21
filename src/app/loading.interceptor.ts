30e2585de9d0d9916f1c994ca20fe342967c4f41
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-10-21 08:14:09 +0100

    Update loading.interceptor.ts

--- src/app/loading.interceptor.ts
+++ src/app/loading.interceptor.ts
@@ -15,21 +15,18 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor(private loadingService: LoadingService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let showLoading = false;
-    if (req.method === 'GET' && this.router.url.includes('/table')) {
+    
       this.loadingService.show();  // Show spinner only if we're on the 'table' route
-      showLoading = true;
       console.log('Loading started on table page for request:', req.url);
-    }
+    
 
 
     return next.handle(req).pipe(
       delay(3000),
       finalize(() => {
-        if(showLoading){
+        
           this.loadingService.hide(); // Hide spinner after request completes
           console.log('request completed')
-        }
       })
     );
   }
